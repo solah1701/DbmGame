b595e5a23bbbc2474fd1c34ecd1e4f6054bf3d63
Stephen Olah <solah1701@gmail.com>
2016-04-24 22:12:15 +1000

    Added a test which returned the cost of a given unit type

---
+++ src/GameEditor/GameCore.UnitTest/DbmUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GameCore.DebellisMultitudinis;
+using GameCore.DebellisMultitudinis.Enumerations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameCore.UnitTest
+{
+    [TestClass]
+    public class DbmUnitTest
+    {
+        [TestMethod]
+        public void When_Unit_Is_General_Non_Ally_Regular_Superior_Knight_Cost_Is_35()
+        {
+            // Arrange
+            const int expected = 35;
+
+            // Act
+            var unit = new Unit
+            {
+                IsGeneral = true,
+                DisciplineType = DisciplineTypeEnum.Regular,
+                GradeType = GradeTypeEnum.Superior,
+                UnitType = UnitTypeEnum.Knights
+            };
+
+            // Assert
+            Assert.AreEqual(expected, unit.Cost);
+        }
+    }
+}

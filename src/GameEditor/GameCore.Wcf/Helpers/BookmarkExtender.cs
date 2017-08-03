d25c4cf57470bc261dc899496cfcd829c448c40c
Stephen Olah <solah1701@gmail.com>
2017-08-03 10:22:30 +1000

    Changes to WCF - testing branches in Git

---
+++ src/GameEditor/GameCore.Wcf/Helpers/BookmarkExtender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameCore.Wcf.Helpers
+{
+    public static class BookmarkExtender
+    {
+        public static bool Matches(this IBookmarkService service, Uri uri, string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
